c535d131f67d5dafe4497935fa6e3100bb98ab87
Fabiana Sugamele <sugamele.fabiana@gmail.com>
2020-10-25 21:04:37 -0300

    projeto finalizado

---
+++ src/database/fakedata.js
@@ -0,0 +1,33 @@
+module.exports = [
+
+    {
+        id: 1,
+        lat: "-23.5500991",
+        lng: "-46.6355097",
+        name: "Lar das meninas",
+        description: "Presta assistência a crianças de 0 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade.",
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar",
+        opening_hours: "Horário de visitas das 8h as 18h",
+        open_on_weekends: "1",
+        images: [
+            "https://images.unsplash.com/photo-1591593443255-db4646e739b4?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1574647258357-e19ec1bcc09c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+        ],
+    },
+
+    {
+        id: 2,
+        lat: "-23.5500991",
+        lng: "-46.6455097",
+        name: "Lar dos meninos",
+        description: "Presta assistência a crianças de 0 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade.",
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar",
+        opening_hours: "Horário de visitas das 8h as 18h",
+        open_on_weekends: "0",
+        images: [
+            "https://images.unsplash.com/photo-1591593443255-db4646e739b4?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1574647258357-e19ec1bcc09c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+
+        ]
+    }
+]
